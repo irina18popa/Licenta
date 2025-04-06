7a95ca514456495f16fed349248dfe2f810d71b0
Irina <irinagabriela.igp@gmail.com>
2025-04-06 23:26:18 +0300

    upnp and tuya transmit device commands

--- backend_python/myenv/bin/activate.fish
+++ backend_python/myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/user/Licenta/myenv
+set -gx VIRTUAL_ENV /home/user/Licenta_git/backend_python/myenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
