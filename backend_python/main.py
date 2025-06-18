42defd1ea3dd61896491888ab644b3be0fe74e4f
Irina <irinagabriela.igp@gmail.com>
2025-06-18 23:36:05 +0300

    done adding dynamic mqtt topics to hub

--- backend_python/main.py
+++ backend_python/main.py
@@ -11,17 +11,20 @@ from testTuya.device import get_tuya_device, get_tuya_device_commands, get_tuya_
 # MQTT broker + topics
 MQTT_BROKER  = "127.0.0.1"
 TOPIC_PUB    = "app/discover/out"
-TOPIC_PUB2   = "app/devices/commands/out"
-TOPIC_PUB3   = "app/devices/status/out"
-TOPIC_PUB4 = "app/devices/state/out"
-TOPIC_PUB5 = "app/devices/do_command/out"
+TOPIC_PUB2 = "app/devices/+/+/out"
+
+# TOPIC_PUB2   = "app/devices/commands/out"
+# TOPIC_PUB3   = "app/devices/status/out"
+# TOPIC_PUB4 = "app/devices/state/out"
+# TOPIC_PUB5 = "app/devices/do_command/out"
 
 
 TOPIC_SUB    = "app/discover/in"
-TOPIC_SUB2   = "app/devices/commands/in"
-TOPIC_SUB3   = "app/devices/status/in"
-TOPIC_SUB4 = "app/devices/state/in"
-TOPIC_SUB5 = "app/devices/do_command/in"
+TOPIC_SUB2 = "app/devices/+/+/in"
+# TOPIC_SUB2   = "app/devices/commands/in"
+# TOPIC_SUB3   = "app/devices/status/in"
+# TOPIC_SUB4 = "app/devices/state/in"
+# TOPIC_SUB5 = "app/devices/do_command/in"
 
 # ───────────────────────────────────────────────────────────────────────
 
@@ -42,16 +45,32 @@ mqtt_client = mqtt.Client()
 
 
 # ───────────────────────────────────────────────────────────────────────
+def parse_topic(topic):
+    parts = topic.split("/")
+    if len(parts) == 5 and parts[0] == "app" and parts[1] == "devices" and parts[4] == "in":
+        return {
+            "deviceId": parts[2],
+            "action": parts[3]
+        }
+    elif len(parts) == 3 and parts[0] == "app" and parts[1] == "discover" and parts[2] == "in":
+        return
+        {
+            "action": parts[1]
+        }
+    return None
+
+
 def on_connect(client, userdata, flags, rc):
     print(f"[Python] Connected to MQTT broker (code {rc})")
     client.subscribe(TOPIC_SUB)  
-    client.subscribe("app/devices/+/+/in")
-
     client.subscribe(TOPIC_SUB2)
-    client.subscribe(TOPIC_SUB3)    # Clear retained on TOPIC_PUB once
-    client.subscribe(TOPIC_SUB4)
-    client.subscribe(TOPIC_SUB5)
+
+    # client.subscribe(TOPIC_SUB2)
+    # client.subscribe(TOPIC_SUB3)    # Clear retained on TOPIC_PUB once
+    # client.subscribe(TOPIC_SUB4)
+    # client.subscribe(TOPIC_SUB5)
     client.publish(TOPIC_PUB, payload=None, retain=True)
+    client.publish(TOPIC_PUB2, payload=None, retain=True)
 
 
 def on_message(client, userdata, msg):
@@ -64,26 +83,37 @@ def on_message(client, userdata, msg):
 
     print(topic)
 
-    if topic == TOPIC_SUB:
-        # schedule discovery task
-        event_loop.call_soon_threadsafe(asyncio.create_task, _do_discover(payload))
+    parsed = parse_topic(topic)
+    if parsed:
+        topic_action = parsed["action"]
 
-    elif topic == TOPIC_SUB2:
-        # schedule handle_commands task
-        event_loop.call_soon_threadsafe(asyncio.create_task, _do_handle_commands(payload))
+        if topic_action == "discover":
+            # schedule discovery task
+            event_loop.call_soon_threadsafe(asyncio.create_task, _do_discover(payload))
+        
+        elif topic_action in ("commands", "status", "state", "do_command"):
+            topic_deviceId = parsed.get("deviceId")
 
-    elif topic == TOPIC_SUB3:
-        # schedule status‐message handling
-        event_loop.call_soon_threadsafe(asyncio.create_task, _handle_status_message(payload))
-    
-    elif topic == TOPIC_SUB4:
-        event_loop.call_soon_threadsafe(asyncio.create_task, _do_handle_state(payload))
+            if not topic_deviceId:
+                print(f"[Python] Missing deviceId for action {topic_action}")
+                return
 
-    elif topic == TOPIC_SUB5:
-        event_loop.call_soon_threadsafe(asyncio.create_task, _do_handle_command_action(payload))
+            if topic_action == "commands":
+                # schedule handle_commands task
+                event_loop.call_soon_threadsafe(asyncio.create_task, _do_handle_commands(payload, topic_deviceId))
 
-    else:
-        print(f"[Python] No handler for topic {topic}")
+            elif topic_action == "status":
+                # schedule status‐message handling
+                event_loop.call_soon_threadsafe(asyncio.create_task, _handle_status_message(payload, topic_deviceId))
+            
+            elif topic_action == "state":
+                event_loop.call_soon_threadsafe(asyncio.create_task, _do_handle_state(payload, topic_deviceId))
+
+            elif topic_action == "do_command":
+                event_loop.call_soon_threadsafe(asyncio.create_task, _do_handle_command_action(payload, topic_deviceId))
+
+        else:
+            print(f"[Python] No handler for topic {topic}")
 
 
 # ───────────────────────────────────────────────────────────────────────
@@ -155,7 +185,7 @@ async def discover_devices(upnp_filter: set = None, tuya_filter: set = None):
 
 
 
-async def _do_handle_command_action(raw: str):
+async def _do_handle_command_action(raw: str, db_device_id: str):
     """
     Parses a JSON payload with 'tuyaID' and 'commands' keys,
     then sends the commands to the Tuya device using do_command().
@@ -184,7 +214,9 @@ async def _do_handle_command_action(raw: str):
             print(f"[ERROR] No payload received for {addr}")
             return
 
-        mqtt_client.publish(TOPIC_PUB4, json.dumps(success), qos=0, retain=False)
+        # it publishes to the state_topic to update the db after the new command
+        pub_topic = f"app/devices/{db_device_id}/state/out"
+        mqtt_client.publish(pub_topic, json.dumps(success), qos=0, retain=False)
 
     except json.JSONDecodeError:
         print("[ERROR] Invalid JSON payload for do_command")
@@ -192,7 +224,7 @@ async def _do_handle_command_action(raw: str):
         print(f"[ERROR] Exception in _do_handle_command_action: {e}")
 
 # ───────────────────────────────────────────────────────────────────────
-async def _do_handle_commands(raw: str):
+async def _do_handle_commands(raw: str, db_device_id:str):
     """
     Runs on the main asyncio loop. Parse raw = "protocol/addr/device_id",
     then await the appropriate command‐fetcher and publish to TOPIC_PUB2.
@@ -211,14 +243,15 @@ async def _do_handle_commands(raw: str):
             print(f"[Python] handle_commands: unsupported protocol '{protocol}'")
             return
 
-        mqtt_client.publish(TOPIC_PUB2, json.dumps(actions), qos=0, retain=False)
+        pub_topic = f"app/devices/{db_device_id}/commands/out"
+        mqtt_client.publish(pub_topic, json.dumps(actions), qos=0, retain=False)
 
     except ValueError:
         print(f"[Python] ERROR: Invalid payload for commands. Expected 'protocol/addr/device_id', got '{raw}'")
 
 
 
-async def _do_handle_state(raw: str):
+async def _do_handle_state(raw: str, db_device_id:str):
     """
     Parses 'protocol/addr' and fetches device state (only for Tuya),
     then publishes it to MQTT topic TOPIC_PUB3.
@@ -238,7 +271,8 @@ async def _do_handle_state(raw: str):
             print(f"[ERROR] No payload received for {addr}")
             return
 
-        mqtt_client.publish(TOPIC_PUB4, json.dumps(state_payload), qos=0, retain=False)
+        pub_topic = f"app/devices/{db_device_id}/state/out"
+        mqtt_client.publish(pub_topic, json.dumps(state_payload), qos=0, retain=False)
 
     except ValueError:
         print(f"[ERROR] Expected 'protocol/addr', got '{raw}'")
@@ -248,7 +282,7 @@ async def _do_handle_state(raw: str):
 
 
 # ───────────────────────────────────────────────────────────────────────
-async def _handle_status_message(raw: str):
+async def _handle_status_message(raw: str, db_device_id:str):
     """
     Runs on the main asyncio loop. Parse raw = "<db_id>/<protocol>/<id_val>/<old_status>",
     register/update monitored_devices & last_status, and ensure status_poll_loop is started once.
@@ -256,9 +290,9 @@ async def _handle_status_message(raw: str):
     global polling_task
 
     try:
-        db_id, protocol, id_val, old_status = raw.split("/")
+        protocol, id_val, old_status = raw.split("/")
         protocol   = protocol.strip().lower()
-        db_id      = db_id.strip()
+        db_id      = db_device_id.strip()
         id_val     = id_val.strip()
         old_status = old_status.strip().lower()
         if old_status not in ("online", "offline"):
@@ -317,7 +351,8 @@ async def status_poll_loop():
                 #aici trebuie sa sterg db_id  din payload
 
                 payload = f"{current}"
-                mqtt_client.publish(TOPIC_PUB3, payload, qos=0, retain=False)
+                pub_topic = f"app/devices/{db_id}/status/out"
+                mqtt_client.publish(pub_topic, payload, qos=0, retain=False)
                 last_status[db_id] = current
 
         await asyncio.sleep(1)
